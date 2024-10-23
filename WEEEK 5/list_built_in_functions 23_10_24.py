cf77a3fb8b2e4a44bf0c7a766ba1fdc22311211f
abduljaneeb <janeebkonami@gmail.com>
2024-10-23 07:38:06 +0100

    Add files via upload

---
+++ WEEEK 5/list_built_in_functions 23_10_24.py
@@ -0,0 +1,6 @@
+list1=[1,2,3,4,5,6,7,8,9,10]
+print("min: ",min(list1))
+print("max: ",max(list1))
+print("count 3: ",list1.count(3))
+print("sort: ",sorted(list1))
+print("reverse sort: ",sorted(list1,reverse=True))
