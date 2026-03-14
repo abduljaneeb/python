68e21f3fa6db49a7df90da23299b5728b8d36577
ABDUL JANEEB K <janeebkonami@gmail.com>
2026-03-14 15:25:18 +0530

    Add Fibonacci series generator in FibonacciSeries.py

---
+++ BASICS/FibonacciSeries.py
@@ -0,0 +1,13 @@
+n = int(input("Enter number of terms: "))
+
+a, b = 0, 1
+
+for i in range(n):
+    print(a)
+    a, b = b, a + bn = int(input("Enter number of terms: "))
+
+a, b = 0, 1
+
+for i in range(n):
+    print(a)
+    a, b = b, a + b
