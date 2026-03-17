b163e74bda9e07874df405a5269959c1066295b6
ABDUL JANEEB K <janeebkonami@gmail.com>
2026-03-17 11:28:21 +0530

    Add files via upload

---
+++ BASICS/List.py
@@ -0,0 +1,14 @@
+list=["one","two","three"]
+
+
+
+#insert to the last postion we using append functionality
+
+list.append("janeeb")
+
+
+#suppose user enter value want to store list we use this
+
+list.append(input("Enter a value : "))
+
+print(list)
