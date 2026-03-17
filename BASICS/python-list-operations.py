e42660a7add9c79bc5cb26000b356cc9b5ffcdfa
ABDUL JANEEB K <janeebkonami@gmail.com>
2026-03-17 11:50:16 +0530

    Create python-list-operations.py

---
+++ BASICS/python-list-operations.py
@@ -0,0 +1,19 @@
+fruits = ["Apple", "Banana", "Grapes"]
+
+empty_list = []
+
+first_item = fruits[0]
+
+subset = fruits[1:3]
+
+
+
+print(fruits)
+
+print(empty_list)
+
+print(first_item)
+
+print(subset)
+
+
