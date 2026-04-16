d77a79c55f4cbe4dec0d81062475d7d90bb88644
ABDUL JANEEB K <janeebkonami@gmail.com>
2026-04-16 13:16:37 +0530

    Update positive_or_negative.py

--- BASICS/positive_or_negative.py
+++ BASICS/positive_or_negative.py
@@ -1,5 +1,8 @@
 num=int(input("Enter a Number : "))
 if num<0 :
     print("entered number is negative")
+elif num == 0 :
+    print("entered number is negative")
 else :
     print("entered number is positive")
+
