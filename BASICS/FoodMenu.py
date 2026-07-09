535af583513f105fccdc2b33df8b750a831f960c
ABDUL JANEEB K <janeebkonami@gmail.com>
2026-07-09 17:31:46 +0530

    Add food menu display and selection logic

---
+++ BASICS/FoodMenu.py
@@ -0,0 +1,31 @@
+print("===============================")
+print("          FOOD MENU            ")
+print("===============================")
+print("1. biriyani          -200")
+print("2. ghee rice         -400")
+print("3. egg               -600")
+print("4. dhosha            -800")
+print("===============================")
+
+food=int(input("Enter a item (1-4) : "))
+
+if food == 1 :
+    print("you selected food is biriyani")
+    print("price-200")
+
+elif food == 2 :
+    print("you selected food is ghee rice")
+    print("price-400")
+    
+elif food == 3 :
+    print("you selected food is egg")
+    print("price-600")
+    
+elif food == 4 :
+    print("you selected food is dhosha")
+    print("price-800")
+    
+else :
+
+    print("you selected invalid choice sorrry")
+        
